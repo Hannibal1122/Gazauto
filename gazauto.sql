e5ef82f995c808322ac4c41c2c9c1002515bdb43
Mikhail Ruchenkin <ruchenkin@vistgroup.ru>
2018-04-26 18:41:30 +0300

    db

---
+++ gazauto.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 26 2018 г., 17:41
+-- Версия сервера: 10.1.28-MariaDB
+-- Версия PHP: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `gazauto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `fields`
+--
+
+CREATE TABLE `fields` (
+  `id` int(11) NOT NULL,
+  `tableId` int(11) NOT NULL,
+  `i` int(11) NOT NULL,
+  `name_column` varchar(32) NOT NULL,
+  `type` varchar(12) NOT NULL,
+  `value` varchar(128) DEFAULT NULL,
+  `state` varchar(128) DEFAULT NULL,
+  `info` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `fields`
+--
+
+INSERT INTO `fields` (`id`, `tableId`, `i`, `name_column`, `type`, `value`, `state`, `info`) VALUES
+(4, 66, 0, 'test', 'head', NULL, NULL, NULL),
+(5, 66, 1, 'adfgsdgf', 'head', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `password`
+--
+
+CREATE TABLE `password` (
+  `login` varchar(32) NOT NULL,
+  `hash` varchar(128) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `password`
+--
+
+INSERT INTO `password` (`login`, `hash`) VALUES
+('admin', '$2a$10$644bb3233e1ff251b4b4eumdZjoiZjWjFLyol.Ad7uUoNWlWCpz.u'),
+('test', '$2a$10$2724eef3ab8d9290e364cuhdQdaRUuHp0sj5Ql6UA0wleUeeEzeIu');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `registration`
+--
+
+CREATE TABLE `registration` (
+  `login` varchar(32) NOT NULL,
+  `role` varchar(32) DEFAULT NULL,
+  `dater` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `registration`
+--
+
+INSERT INTO `registration` (`login`, `role`, `dater`) VALUES
+('admin', 'Администратор', '2017-12-25 15:57:01'),
+('test', 'Инженер', '2018-04-20 13:59:03');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rights`
+--
+
+CREATE TABLE `rights` (
+  `id` int(11) NOT NULL,
+  `objectId` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `login` varchar(64) NOT NULL,
+  `rights` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `rights`
+--
+
+INSERT INTO `rights` (`id`, `objectId`, `type`, `login`, `rights`) VALUES
+(27, 30, 'user', 'test', 1),
+(29, 66, 'user', 'test', 1),
+(70, 32, 'role', 'Инженер', 9),
+(71, 32, 'user', 'test', 1),
+(87, 92, 'user', 'test', 1),
+(88, 93, 'user', 'test', 1),
+(92, 91, 'user', 'test', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `role` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`role`) VALUES
+('Администратор'),
+('Инженер'),
+('Менеджер');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `signin`
+--
+
+CREATE TABLE `signin` (
+  `id` varchar(32) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `checkkey` varchar(32) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `signin`
+--
+
+INSERT INTO `signin` (`id`, `login`, `checkkey`, `date`) VALUES
+('G6uhkRxY834', 'test', 'b07f41e73c3f7f9bf11693998491efeb', '2018-04-25 15:52:27'),
+('GJ3COg64AOP', 'admin', '176d8c6582c7d758e1253ddd345ec60f', '2018-04-26 17:38:30');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `structures`
+--
+
+CREATE TABLE `structures` (
+  `id` int(11) NOT NULL,
+  `objectType` varchar(10) NOT NULL,
+  `objectId` int(11) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `parent` int(11) NOT NULL,
+  `priority` int(11) DEFAULT '0',
+  `info` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `structures`
+--
+
+INSERT INTO `structures` (`id`, `objectType`, `objectId`, `name`, `parent`, `priority`, `info`) VALUES
+(30, 'folder', 0, 'База знаний', 0, 0, ''),
+(32, 'folder', 0, 'Проекты', 0, 0, ''),
+(33, 'folder', 0, 'Шаблоны', 0, 0, ''),
+(66, 'table', 0, 'Новая таблица', 30, 0, ''),
+(67, 'table', 0, 'Новая таблица', 30, 0, ''),
+(68, 'folder', 0, 'Новая папка', 30, 0, ''),
+(70, 'table', 0, 'Новая таблица 1', 68, 0, ''),
+(71, 'table', 0, 'Новая таблица 2', 68, 0, ''),
+(91, 'folder', 0, 'Папка для test', 30, 0, ''),
+(92, 'folder', 0, 'Администрирование', 0, 0, ''),
+(93, 'folder', 0, 'Пользователи', 92, 0, ''),
+(94, 'folder', 0, 'Роли', 92, 0, '');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `fields`
+--
+ALTER TABLE `fields`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `password`
+--
+ALTER TABLE `password`
+  ADD PRIMARY KEY (`login`);
+
+--
+-- Индексы таблицы `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`login`);
+
+--
+-- Индексы таблицы `rights`
+--
+ALTER TABLE `rights`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role`);
+
+--
+-- Индексы таблицы `signin`
+--
+ALTER TABLE `signin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `structures`
+--
+ALTER TABLE `structures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `fields`
+--
+ALTER TABLE `fields`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `rights`
+--
+ALTER TABLE `rights`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+
+--
+-- AUTO_INCREMENT для таблицы `structures`
+--
+ALTER TABLE `structures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
