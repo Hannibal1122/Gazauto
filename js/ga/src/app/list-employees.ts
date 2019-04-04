6ca9c38b4c627a162d7372b65c3c9f3d676bc0ad
guzimak <guzel.maksutova@yandex.ru>
2019-04-04 15:52:12 +0300

    апрель четвертое

--- js/ga/src/app/list-employees.ts
+++ js/ga/src/app/list-employees.ts
@@ -3,15 +3,65 @@ import { Employees } from './employees';
 
 export const EMPLOYEES: Employees[] = [
     { tabelnom: 1, surname: 'Петров', name: 'Петр', patronymic: 'Петрович', 
-        days: [ { color: 'green', value: 'От', colspan: 4}, { color: 'green', value: 'От', colspan: 4}, { color: 'green', value: 'От', colspan: 1}, { color: 'green', value: 'От', colspan: 1}, { color: 'gray', value: 'Вых', colspan: 1}, { color: 'red', value: 'Вых', colspan: 1}, { color: 'green', value: '8', colspan: 1}, { color: 'gray', value: '8', colspan: 1}, { color: 'red', value: 'К', colspan: 1}, { color: 'green', value: 'К', colspan: 1}, { color: 'gray', value: 'К', colspan: 1}, { color: 'red', value: 'К', colspan: 1}, { color: 'green', value: 'К', colspan: 1}, { color: 'gray', value: 'К', colspan: 1}, { color: 'red', value: 'К', colspan: 1}, { color: 'green', value: 'К', colspan: 1}, { color: 'gray', value: 'Вых', colspan: 1}, { color: 'red', value: 'Вых', colspan: 1}, { color: 'red', value: '8', colspan: 1}, { color: 'green', value: '8', colspan: 1}, { color: 'gray', value: '8', colspan: 1}, { color: 'red', value: '8', colspan: 1}, { color: 'green', value: '8', colspan: 1}, { color: 'gray', value: 'Вых', colspan: 1}, { color: 'red', value: 'Вых', colspan: 1}, { color: 'green', value: '8', colspan: 1}, { color: 'gray', value: 'Вых', colspan: 1}, { color: 'red', value: '8', colspan: 1}, { color: 'green', value: '8', colspan: 1}, { color: 'gray', value: '8', colspan: 1}, { color: 'red', value: '8', colspan: 1} ] },
-    /*{ tabelnom: 2, surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', 
-        days: [ { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'gray', value: 'Вых'}, { color: 'green', value: '8'}, { color: 'red', value: 'К'}, { color: 'red', value: 'К'} ] },
+        days: [ 
+            { value: 'От', colspan: 4}, 
+            { value: 'Вых', colspan: 2},
+            { value: '8', colspan: 2},  
+            { value: 'К', colspan: 10}, 
+            { value: '8', colspan: 5}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 5}, 
+            { value: 'Вых', colspan: 1} 
+        ] },
+    { tabelnom: 2, surname: 'Иванов', name: 'Иван', patronymic: 'Иванович', 
+        days: [ 
+            { value: '8', colspan: 4}, 
+            { value: 'Вых', colspan: 2},
+            { value: '8', colspan: 5},  
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 5}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: 'К', colspan: 5}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 4}
+        ] },
     { tabelnom: 3, surname: 'Павлов', name: 'Павел', patronymic: 'Павлович', 
-        days: [ { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'} ] },
+        days: [ 
+            { value: '8', colspan: 4}, 
+            { value: 'Вых', colspan: 2},
+            { value: '8', colspan: 5},  
+            { value: 'К', colspan: 7}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 3}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 1}, 
+            { value: 'Вых', colspan: 1}, 
+            { value: '8', colspan: 4}
+        ] },
     { tabelnom: 4, surname: 'Александров', name: 'Александр', patronymic: 'Александрович', 
-        days: [ { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'red', value: 'К'} ] },
+        days: [ 
+            { value: 'К', colspan: 4}, 
+            { value: 'Вых', colspan: 2},
+            { value: 'К', colspan: 5},  
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 5}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 1}, 
+            { value: 'Вых', colspan: 1}, 
+            { value: '8', colspan: 4},
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 3}
+        ] },    
     { tabelnom: 5, surname: 'Егоров', name: 'Егор', patronymic: 'Егорович', 
-        days: [ { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'green', value: '8'}, { color: 'gray', value: 'Вых'}, { color: 'red', value: 'К'}, { color: 'red', value: 'К'} ] }
-*/];
-
-
+        days: [ 
+            { value: '8', colspan: 4}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: 'К', colspan: 10}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 5}, 
+            { value: 'Вых', colspan: 2}, 
+            { value: '8', colspan: 1}, 
+            { value: 'Вых', colspan: 1}, 
+            { value: '8', colspan: 4}
+        ] }
+];
